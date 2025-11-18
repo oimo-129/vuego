b965d35a6a5599588c5803490feac24ced8e4bf2
朱德成 <81302913+oimo-129@users.noreply.github.com>
2025-11-18 21:23:14 +0800

    快完成到登录模块了

--- vue3-rabbit/src/views/Test/index.vue
+++ vue3-rabbit/src/views/Test/index.vue
@@ -1,77 +1,27 @@
-
 <script lang="ts" setup>
-import {
-  Check,
-  Delete,
-  Edit,
-  Message,
-  Search,
-  Star,
-} from '@element-plus/icons-vue'
+//双向绑定
+import { ref } from 'vue';
+const input = ref('')
 
-import ImageView from '@/components/ImageView.vue';
+const handleClick = () => {
+  console.log('当前输入框的值为:', input.value);
+}
 </script>
 
 <template>
-
-  <image-view/>
-  <div class="button-example">
-    <div class="button-row">
-      <el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
-    </div>
-
-    <div class="button-row">
-      <el-button plain>Plain</el-button>
-      <el-button type="primary" plain>Primary</el-button>
-      <el-button type="success" plain>Success</el-button>
-      <el-button type="info" plain>Info</el-button>
-      <el-button type="warning" plain>Warning</el-button>
-      <el-button type="danger" plain>Danger</el-button>
-    </div>
-
-    <div class="button-row">
-      <el-button round>Round</el-button>
-      <el-button type="primary" round>Primary</el-button>
-      <el-button type="success" round>Success</el-button>
-      <el-button type="info" round>Info</el-button>
-      <el-button type="warning" round>Warning</el-button>
-      <el-button type="danger" round>Danger</el-button>
-    </div>
-
-    <div class="button-row">
-      <el-button :icon="Search" circle />
-      <el-button type="primary" :icon="Edit" circle />
-      <el-button type="success" :icon="Check" circle />
-      <el-button type="info" :icon="Message" circle />
-      <el-button type="warning" :icon="Star" circle />
-      <el-button type="danger" :icon="Delete" circle />
-    </div>
+  <div class="container">
+    <el-input v-model="input" style="width: 240px" placeholder="测试双向绑定" />
+    <el-button type="primary" @click="handleClick">测试</el-button>
   </div>
-
+  
 </template>
 
 
 <style scoped>
-.button-example {
+.container {
   display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  margin-top: 20px;
-}
-
-.button-row {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
+  justify-content: center;
   align-items: center;
-}
-
-.button-row > * {
-  margin: 0;
+  gap: 10px;
 }
 </style>
